ac9ac97e8b4ab035c0d1f71078dbcd72d90bf6af
Grzesiek <gromaniuk0@gmail.com>
2023-10-23 12:11:37 +0200

    Add project files.

---
+++ Client/Helpers/StringUtilities.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorApp1.Client.Helpers
+{
+        public class StringUtilities
+        {
+            public static string CustomToUpper(string value) => value.ToUpper();
+        }
+    
+}
