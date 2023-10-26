321e6e8035bea89f32af5a34ebddbf9b5cbc6264
Grzesiek <gromaniuk0@gmail.com>
2023-10-26 14:38:19 +0200

    Added JS functions

---
+++ Client/Helpers/USRuntimeExtensionMethods.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.JSInterop;
+
+namespace BlazorApp1.Client.Helpers
+{
+    public static class USRuntimeExtensionMethods
+    {
+        public static async ValueTask<bool> Confirm(this IJSRuntime js, string message)
+        {
+            await js.InvokeVoidAsync("console.log", "example message");
+            return await  js.InvokeAsync<bool>("confirm", message);
+        }
+        public static async ValueTask MyFunction(this IJSRuntime js, string message)
+        {
+            await js.InvokeVoidAsync("my_function", message);
+        }
+    }
+}
