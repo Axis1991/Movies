72b7421f9d2f8370e67b38a8d80befabb35c7e87
Grzesiek <gromaniuk0@gmail.com>
2023-10-30 13:04:45 +0100

    Added posters and removed test functionalities

--- Client/Helpers/RepositoryInMemory.cs
+++ Client/Helpers/RepositoryInMemory.cs
@@ -1,4 +1,5 @@
 ﻿using Movies.Shared.Entities;
+using static System.Net.WebRequestMethods;
 
 namespace BlazorApp1.Client.Helpers
 {
@@ -7,10 +8,10 @@ namespace BlazorApp1.Client.Helpers
         public List<Movie> GetMovies()
         {
             return new List<Movie>()
-        {
-            new Movie() { Title = "spiderman: far from home", ReleaseDate = new DateTime(2019, 7, 2) },
-            new Movie() { Title = "moana", ReleaseDate = new DateTime(2016, 11, 23) },
-            new Movie() {Title = "inception", ReleaseDate = new DateTime(2010, 7, 16)}
+            {
+            new Movie() { Title = "Avatar: The way of water", ReleaseDate = new DateTime(2022, 12, 16), Poster="https://m.media-amazon.com/images/I/71dcqYNTKmL._AC_UF1000,1000_QL80_.jpg" },
+            new Movie() { Title = "Lord of the Rings", ReleaseDate = new DateTime(2001, 02, 15) , Poster="https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_.jpg"},
+            new Movie() {Title = "Les Misérables", ReleaseDate = new DateTime(2013, 01, 25), Poster="https://fwcdn.pl/fpo/95/95/599595/7504935.3.jpg"}
         };
         }
     }
--- Client/Pages/Counter.razor.cs
+++ Client/Pages/Counter.razor.cs
@@ -7,45 +7,10 @@ namespace BlazorApp1.Client.Pages
 {
     public partial class Counter
     {
-        [Inject] SingletonService singleton {  get; set; }
-        [Inject] TransientService transient {  get; set; }
-        [Inject] IJSRuntime js { get; set; }
-        [CascadingParameter] public AppState AppState { get; set; }
-
-        private List<Movie> movies;
-        protected override void OnInitialized()
-        {
-            movies = new List<Movie>()
-        {
-            new Movie(){Title = "Joker", ReleaseDate = new DateTime(2021, 8, 12)},
-            new Movie(){Title = "Avengers: End Game", ReleaseDate = new DateTime(2016, 11, 23)},
-            new Movie(){Title = "Piccolo", ReleaseDate = new DateTime(2013, 1, 16)}
-        };
-        }
-
         private int currentCount = 0;
-        private static int currentCountStatic = 0;
-
-        [JSInvokable]
-        public async Task IncrementCount()
+        public void IncrementCount()
         {
             currentCount++;
-            singleton.Value += 1;
-            transient.Value += 1;
-            currentCountStatic++;
-            js.InvokeVoidAsync("dotnetStaticInvocation");
-        }
-
-        private async Task IncrementCountJavaScript()
-        {
-            await js.InvokeVoidAsync("dotnetInstanceInvocation", DotNetObjectReference.Create(this));
-        }
-
-
-        [JSInvokable]
-        public static Task<int> GetCurrentCount()
-        {
-            return Task.FromResult(currentCountStatic);
         }
     }
 }
