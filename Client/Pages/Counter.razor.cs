0a68422e5f711aae7f2d4c454c55e2be54035c5d
Grzesiek <gromaniuk0@gmail.com>
2023-10-30 18:11:40 +0100

    Added lazy loading - one js. related bug to be resolved

--- Client/Pages/Counter.razor.cs
+++ Client/Pages/Counter.razor.cs
@@ -1,34 +1,47 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using MathNet.Numerics.Optimization;
+using MathNet.Numerics.Statistics;
+using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 using Movies.Shared.Entities;
 using static BlazorApp1.Client.Shared.MainLayout;
-using MathNet.Numerics.Statistics;
-using System.Security.Cryptography;
 
 namespace BlazorApp1.Client.Pages
 {
-
     public partial class Counter
     {
+     
         [Inject] IJSRuntime js { get; set; }
+ 
 
         private int currentCount = 0;
         private static int currentCountStatic = 0;
         IJSObjectReference module;
-
         [JSInvokable]
         public async Task IncrementCount()
         {
+
             var array = new double[] { 1, 2, 3, 4, 5 };
             var max = array.Maximum();
             var min = array.Minimum();
 
-            module = await js.InvokeAsync<IJSInProcessObjectReference>("import", "./js/Counter.js");
-            await module.InvokeVoidAsync("displayAlert", $"Max is {max} and min is {min}");
+            module = await js.InvokeAsync<IJSObjectReference>("import", "./js/Counter.js");
+            await module.InvokeVoidAsync("displayAlert", $"Maxi is {max} and min is {min}");
 
             currentCount++;
             currentCountStatic++;
             await js.InvokeVoidAsync("dotnetStaticInvocation");
+        }
+
+        private async Task IncrementCountJavaScript()
+        {
+            await js.InvokeVoidAsync("dotnetInstanceInvocation", DotNetObjectReference.Create(this));
+        }
+
+
+        [JSInvokable]
+        public static Task<int> GetCurrentCount()
+        {
+            return Task.FromResult(currentCountStatic);
         }
     }
 }
