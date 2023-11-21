fd0ed6dfdd708e14eabe5bdb3084d11174cab399
Grzesiek <gromaniuk0@gmail.com>
2023-11-21 16:15:51 +0100

    Added custom auth message

--- Client/Auth/DummyAuthStateProvider.cs
+++ Client/Auth/DummyAuthStateProvider.cs
@@ -7,6 +7,7 @@ namespace BlazorApp1.Client.Auth
     {
         public async override Task<AuthenticationState> GetAuthenticationStateAsync()
         {
+            await Task.Delay(3000);
             var annonymous = new ClaimsIdentity();
             return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(annonymous)));
         }
