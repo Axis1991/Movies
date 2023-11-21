ff3efd7f7958ea412374dd72910c4d379fd6ffea
Grzesiek <gromaniuk0@gmail.com>
2023-11-21 16:06:46 +0100

    Added authentication component and state provider

---
+++ Client/Auth/DummyAuthStateProvider.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace BlazorApp1.Client.Auth
+{
+    public class DummyAuthStateProvider : AuthenticationStateProvider
+    {
+        public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var annonymous = new ClaimsIdentity();
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(annonymous)));
+        }
+    }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,5 +1,7 @@
 using BlazorApp1.Client;
+using BlazorApp1.Client.Auth;
 using BlazorApp1.Client.Helpers;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using System.Runtime.CompilerServices;
@@ -8,7 +10,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 ConfigureServices(builder.Services);
 
@@ -18,4 +20,6 @@ await builder.Build().RunAsync();
 {
     services.AddTransient<Irepository, RepositoryInMemory>();
     services.AddAuthorizationCore();
+    services.AddScoped<AuthenticationStateProvider, DummyAuthStateProvider>();
+
 }
