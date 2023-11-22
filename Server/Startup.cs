5c3b30ba52b24d85c38a3e0abdd4530718e5d3e4
Grzesiek <gromaniuk0@gmail.com>
2023-11-22 16:58:24 +0100

    - Added Identity service - Changes to db context - Database update after Identity migration

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -1,61 +1,81 @@
-﻿//using Microsoft.AspNetCore.Builder;
-//using Microsoft.AspNetCore.Hosting;
-//using Microsoft.AspNetCore.HttpsPolicy;
-//using Microsoft.AspNetCore.ResponseCompression;
-//using Microsoft.EntityFrameworkCore;
-//using Microsoft.Extensions.Configuration;
-//using Microsoft.Extensions.DependencyInjection;
-//using Microsoft.Extensions.Hosting;
-//using System.Linq;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Linq;
 
-//namespace Movies.Server
-//{
-//    public class Startup
-//    {
-//        public Startup(IConfiguration configuration)
-//        {
-//            Configuration = configuration;
-//        }
+namespace Movies.Server
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
 
-//        public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; }
 
-//        // This method gets called by the runtime. Use this method to add services to the container.
-//        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-//        public void ConfigureServices(IServiceCollection services)
-//        {
-//            services.AddDbContext<ApplicationDbContext>(options =>
-//            options.UseSqlServer(Configuration.GetConnectionString("LocalMoviesDB")));
-//            services.AddControllersWithViews();
-//            services.AddRazorPages();
-//        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("LocalMoviesDB")));
+            services.AddControllersWithViews();
+            services.AddRazorPages();
 
-//        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-//        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-//        {
-//            if (env.IsDevelopment())
-//            {
-//                app.UseDeveloperExceptionPage();
-//                app.UseWebAssemblyDebugging();
-//            }
-//            else
-//            {
-//                app.UseExceptionHandler("/Error");
-//                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//                app.UseHsts();
-//            }
+            services.AddIdentity<IdentityUser, IdentityRole>()
+               .AddEntityFrameworkStores<ApplicationDbContext>()
+               .AddDefaultTokenProviders();
 
-//            app.UseHttpsRedirection();
-//            app.UseBlazorFrameworkFiles();
-//            app.UseStaticFiles();
+            //services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            //    .AddJwtBearer(options =>
+            //    options.TokenValidationParameters = new TokenValidationParameters
+            //    {
+            //        ValidateIssuer = false,
+            //        ValidateAudience = false,
+            //        ValidateLifetime = true,
+            //        ValidateIssuerSigningKey = true,
+            //        IssuerSigningKey = new SymmetricSecurityKey(
+            //        Encoding.UTF8.GetBytes(Configuration["jwt:key"])),
+            //        ClockSkew = TimeSpan.Zero
+            //    });
 
-//            app.UseRouting();
+        }
 
-//            app.UseEndpoints(endpoints =>
-//            {
-//                endpoints.MapRazorPages();
-//                endpoints.MapControllers();
-//                endpoints.MapFallbackToFile("index.html");
-//            });
-//        }
-//    }
-//}
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseWebAssemblyDebugging();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseBlazorFrameworkFiles();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+                endpoints.MapControllers();
+                endpoints.MapFallbackToFile("index.html");
+            });
+        }
+    }
+}
